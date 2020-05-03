544f9a48a2aa0cf5cb388334eaba8d02f49d3928
Roberio Praciano <roberio.praciano@gmail.com>
2020-05-03 18:28:10 -0300

    Correcao de testes

--- src/App.js
+++ src/App.js
@@ -4,46 +4,42 @@ import api from './services/api';
 import "./styles.css";
 
 function App() {
-  const [projects, setProjects] = useState([]);
+  const [repositories, setRepositories] = useState([]);
  
   useEffect(() => {
-    api.get('/projects').then(response => { setProjects(response.data) })
+    api.get('repositories').then(response => { setRepositories(response.data) })
   }, []);
 
   async function handleAddRepository() {
-    const response = await api.post('projects', {
+    const rand = Math.floor(Math.random() * 1000) + 3
+    const response = await api.post('repositories', {
+      id: `${rand}`,
       title: `Novo Projeto ${Date.now()}`,
       owner: "Roberio Praciano"
     });
-    const project = response.data;
+    const repositorie = response.data;
 
-    setProjects([...projects, project]);
+    setRepositories([...repositories, repositorie]);
   }
 
   async function handleRemoveRepository(id) {
     console.log(`Delete ${id}`)
-    const response = await api.delete(`projects/${id}` );
-     
-    const projectIndex = projects.findIndex(project => project.id === id);
-    
-    const projectsnew = projects;
-    
-
-    projectsnew.splice(projectIndex, 1);
-
-    setProjects([...projectsnew]);
+    const response = await api.delete(`repositories/${id}` );
+    const repositorieIndex = repositories.findIndex(repositories => repositories.id === id);
+    const repositorienew = repositories;
+    repositorienew.splice(repositorieIndex, 1);
+    console.table(repositorienew);
+    setRepositories([...repositorienew]);
   };
 
   return (
     <div>
       <ul data-testid="repository-list">
-              {projects.map(project => <li key={project.id}>{project.title}
-          
-              <button onClick={() => handleRemoveRepository(project.id)}>
+        {repositories.map(repositorie => <li key={repositorie.id}>{repositorie.title}
+          <button onClick={() => handleRemoveRepository(repositorie.id)}>
                   Remover
                </button>   </li>
-                  )};
-         
+                  )}
       </ul>
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
